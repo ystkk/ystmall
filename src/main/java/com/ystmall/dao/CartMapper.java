f2865e8e054e9acb01c566c9ae196978458267ec
yst <974681864@qq.com>
2018-12-08 23:38:16 +0800

    project init

---
+++ src/main/java/com/ystmall/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package com.ystmall.dao;
+
+import com.ystmall.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
---
+++ src/main/java/com/ystmall/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.ystmall.dao;
+
+import com.ystmall.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
